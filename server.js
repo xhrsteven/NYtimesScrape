371c23b1375a8973d337542d42b66b420e1b33a2
DESKTOP-DE5BIT9\hxx14 <xhrsteven@gmail.com>
2018-07-14 15:06:52 -0500

    no message

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var exphbs = require('express-handlebars');
 var request =require('request');
 var cheerio = require('cheerio');
 
-var db= require('./models');
+var db= require(path.resolve('__dirname','./models'));
 
 var PORT = process.env.PORT || 8080;
 
